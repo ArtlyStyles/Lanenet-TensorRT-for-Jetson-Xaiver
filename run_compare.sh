969ce43e4218c5e7888dba9744cc783995ef3e09
Artly <userstyles@artlystyles.com>
2019-11-16 19:13:52 -0800

    Delete run_compare.sh

--- run_compare.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=~/car/apps/lanenet-lane-detection
-python3 tools/compare_lanenet.py --weights_path ./model/tusimple_lanenet/tusimple_lanenet_vgg.ckpt --image_path ./data/tusimple_test_image/0_512_256.jpg
